33ff39a0f0e39b3d046ba446e05d00fdf48fd44d
baiyuhang <734004037@qq.com>
2023-12-10 18:34:10 +0800

    feat: 合同顶部搜索

--- src/pages/contract/index.jsx
+++ src/pages/contract/index.jsx
@@ -1,19 +1,26 @@
-import { useMemo } from 'react'
-import Taro, { useDidShow } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { useMemo, useState } from "react";
+import Taro, { useDidShow } from "@tarojs/taro";
+import { View, Text } from "@tarojs/components";
+import { AtSearchBar } from "taro-ui";
 
+export default function Index() {
+  const [searchWord, setSearchWord] = useState("");
 
-export default function Index () {
-  const page = useMemo(() => Taro.getCurrentInstance().page, [])
+  const page = useMemo(() => Taro.getCurrentInstance().page, []);
 
   useDidShow(() => {
-    const tabbar = Taro.getTabBar(page)
-    tabbar?.setSelected(3)
-  })
+    const tabbar = Taro.getTabBar(page);
+    tabbar?.setSelected(3);
+  });
+
+  const onChange = (value) => {
+    setSearchWord(value);
+  };
 
   return (
     <View className="index min-h-[100vh] bg-[#f6f6f6]">
-      <Text>我是个人中心！</Text>
+      <AtSearchBar value={searchWord} onChange={onChange} />
+      <View>{searchWord}</View>
     </View>
-  )
+  );
 }
