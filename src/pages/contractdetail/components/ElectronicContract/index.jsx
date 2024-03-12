5d810bdbfd488b665a6b60ee2b72f9c0ef08ecf5
leyuan <1577142422@qq.com>
2024-03-12 23:05:20 +0800

    feat: 合同详情页面

--- src/pages/contractdetail/components/ElectronicContract/index.jsx
+++ src/pages/contractdetail/components/ElectronicContract/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { View, CoverImage, Picker } from "@tarojs/components";
+import { Button } from "@antmjs/vantui";
 
 export default function Index() {
   const [selector] = useState(["美国", "中国", "巴西", "日本"]);
@@ -10,31 +11,57 @@ export default function Index() {
   };
 
   return (
-    <View className="index flex justify-between py-[20px] px-[30px] bg-white border-t border-[#979797] border-opacity-10">
-      <View>筛选:</View>
-      <View className="relative inline-block">
-        <Picker mode="selector" range={selector} onChange={onChange}>
-          <View className="flex items-center">
-            <View>{selectorChecked || "保险状态"}</View>
-            <CoverImage
-              className="ml-[4px] w-[20px]"
-              src="https://jiazheng.bj.bcebos.com/sanjiao.png"
-            />
+    <>
+      <View className="p-[32px] bg-white m-[32px] mb-[32px] rounded-[12px]">
+        <View className="w-full">
+          <View className="mb-[48px]">电子合同（0）</View>
+          <View className="flex flex-col items-center justify-center">
+            <View className="text-[24px] text-gray-400">还没有签约的合同</View>
+            <View className="text-[24px] text-gray-400 mb-[16px]">
+              可点击下方签署远程按钮
+            </View>
+            <Button className="m-0 mb-[32px]" color="#1d20a4">
+              +签署远程电子合同
+            </Button>
           </View>
-        </Picker>
+        </View>
       </View>
-      <View className="relative inline-block">
-        <Picker mode="selector" range={selector} onChange={onChange}>
-          <View className="flex items-center">
-            <View>{selectorChecked || "电子合同"}</View>
-            <CoverImage
-              className="ml-[4px] w-[20px]"
-              src="https://jiazheng.bj.bcebos.com/sanjiao.png"
-            />
+      {/*<View className="p-[32px] bg-white m-[32px] mb-[32px] rounded-[12px]">
+        <View className="w-full">
+          <View className="mb-[32px] flex items-center justify-between">
+            <View>电子合同（1）</View>
+            <Button className="m-0" color="#1d20a4">
+              +创建电子合同
+            </Button>
           </View>
-        </Picker>
+          <View className="flex flex-col border border-gray-300 border-solid rounded-[8px] p-[32px]">
+            <View className="mb-[24px]">通用版</View>
+            <View className="text-[24px] text-gray-400 mb-[8px]">
+              门店：未签约
+            </View>
+            <View className="text-[24px] text-gray-400 mb-[8px]">
+              客户：（未签约）
+            </View>
+            <View className="text-[24px] text-gray-400 mb-[16px]">
+              家政员：（未签约）
+            </View>
+            <View className="flex justify-between">
+              <View className="text-[24px] text-gray-400">YY 2023</View>
+              <View className="text-[24px] text-gray-400">实名认证签署</View>
+            </View>
+          </View>
+        </View>
+  </View> */}
+      <View className="p-[32px] bg-white m-[32px] mb-[32px] rounded-[12px]">
+        <View className="w-full">
+          <View className="mb-[48px]">上传纸质合同图片（0）</View>
+          <View>
+            <Button className="m-0" color="#1d20a4">
+              添加照片
+            </Button>
+          </View>
+        </View>
       </View>
-      <View>快到期/刚签约</View>
-    </View>
+    </>
   );
 }
--- src/pages/contractdetail/index.jsx
+++ src/pages/contractdetail/index.jsx
@@ -2,12 +2,12 @@ import { getContractDetail } from "@/api";
 import { useEffect, useState } from "react";
 import { View } from "@tarojs/components";
 import { useRouter } from "@tarojs/taro";
-import { AtTabs, AtTabsPane, AtButton } from "taro-ui";
+import { AtTabs, AtTabsPane } from "taro-ui";
 import moment from "moment";
+import { Button } from "@antmjs/vantui";
 
 import ElectronicContract from "./components/ElectronicContract";
 import "./index.scss";
-import { Button } from "@antmjs/vantui";
 
 export default function Index() {
   const [tabCurrent, setTabCurrent] = useState(0);
@@ -53,7 +53,9 @@ export default function Index() {
         <View className="base-detail bg-white rounded-t-[16px]">
           <View className="title-line py-[19px] px-[32px] flex justify-between text-[30px]">
             <View>{contractData.contractTypeShow}合同</View>
-            <View>{contractData.contractStateShow}</View>
+            <View className="text-[#eab939]">
+              {contractData.contractStateShow}
+            </View>
           </View>
           <View className="px-[32px] py-[19px] text-[26px] text-black/50">
             (合同编号 2023080112300)
@@ -67,7 +69,9 @@ export default function Index() {
           <View className="px-[32px] py-[19px] flex justify-between text-[30px]">
             <View>客户</View>
             <View>
-              {contractData.customerName + " " + contractData.userPhone || ""}
+              {contractData.customerName +
+                " " +
+                (contractData?.userPhone || "")}
             </View>
           </View>
           <View className="px-[32px] py-[19px] flex justify-between text-[30px]">
@@ -90,7 +94,7 @@ export default function Index() {
             <ElectronicContract></ElectronicContract>
           </AtTabsPane>
           <AtTabsPane current={tabCurrent} index={1}>
-            <View className="p-[32px] flex flex-col gap-[32px] bg-white m-[32px]">
+            <View className="p-[32px] flex flex-col gap-[32px] bg-white m-[32px] rounded-[8px]">
               <View className="flex justify-between items-center">
                 <View>合同展示内容</View>
                 <Button className="m-0" color="#1d20a4">
@@ -132,7 +136,45 @@ export default function Index() {
             </View>
           </AtTabsPane>
           <AtTabsPane current={tabCurrent} index={2}>
-            {/* <Expense></Expense> */}
+            <View className="p-[32px] flex flex-col gap-[32px] bg-white m-[32px] rounded-[8px]">
+              <View>客户：{contractData.customerName}</View>
+              <View className="p-[32px] bg-[#f5f6f8] mb-[32px] rounded-[8px] flex items-center justify-evenly">
+                <View className="flex flex-col items-center">
+                  <View>0.00</View>
+                  <View className="text-[24px] text-gray-400">向他/她收取</View>
+                </View>
+                <View className="flex flex-col items-center">
+                  <View>0.00</View>
+                  <View className="text-[24px] text-gray-400">向他/她支出</View>
+                </View>
+                <View className="flex flex-col items-center">
+                  <View>0.00</View>
+                  <View className="text-[24px] text-gray-400">结余</View>
+                </View>
+              </View>
+            </View>
+            <View className="p-[32px] flex flex-col gap-[32px] bg-white m-[32px] rounded-[8px]">
+              <View className="flex gap-[8px]">
+                <View>家政员：{contractData.customerName} </View>
+                <View className="text-[24px] text-gray-400">
+                  （工资：{contractData.monthlySalary}）
+                </View>
+              </View>
+              <View className="flex gap-[8px]">
+                <View className="text-[24px] text-gray-400">上下户时间：</View>
+                {contractData.conStartDate + "至" + contractData.conEndDate}
+              </View>
+              <View className="p-[32px] bg-[#f5f6f8] mb-[32px] rounded-[8px] flex items-center justify-evenly">
+                <View className="flex flex-col items-center">
+                  <View>0.00</View>
+                  <View className="text-[24px] text-gray-400">向他/她收取</View>
+                </View>
+                <View className="flex flex-col items-center">
+                  <View>0.00</View>
+                  <View className="text-[24px] text-gray-400">向他/她支出</View>
+                </View>
+              </View>
+            </View>
           </AtTabsPane>
         </AtTabs>
       </View>
